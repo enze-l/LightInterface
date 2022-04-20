1d77b57dbf4f892ed1701414c96b14e2919019c8
enze-l <leon.enzenberger@protonmail.com>
2022-04-20 16:22:01 +0200

    Utilize current brightness

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import {AreaChart, XAxis, YAxis, Area} from "recharts";
+import {XAxis, YAxis, Area, Label, ComposedChart} from "recharts";
+import {Box, Slider} from "@mui/material";
 import axios from "axios";
 import React from "react";
 
 const serverAddress = "http://localhost:3000"
 const textColor = "#6B7280"
-const fillColor = "#dbdbdb"
+const graphColor = "#ffffff"
 const buttonStyle = "m-1 border border-gray-500 hover:bg-gray-700 text-gray-500 font-bold py-2 px-4 rounded-full"
 
 function App() {
     const [hundredValues, setHundredValues] = React.useState([])
+    const [brightnessValue, setBrightnessValue] = React.useState(0)
 
     React.useEffect(() => {
         axios.get(serverAddress + "/sensor/100")
@@ -16,10 +18,18 @@ function App() {
                 const array = String(res.data).split(/(\s+)/).filter(e => e.trim().length > 0)
                 setHundredValues(convertArrayToObjects(array))
             })
+        axios.get(serverAddress + "/display/brightness")
+            .then(res => {
+                const value = res.data * 100
+                setBrightnessValue(value)
+                console.log("Current brightness value = " + res.data)
+            })
     }, [])
 
-    function setBrightness(data){
-        axios.post(serverAddress + "/display/brightness", data).then()
+    function setBrightness(dataPercent) {
+        axios.post(serverAddress + "/display/brightness", dataPercent / 100).then(res => {
+            console.log(dataPercent)
+        })
     }
 
     function convertArrayToObjects(array) {
@@ -32,20 +42,33 @@ function App() {
         <div className="App">
             <header className="App-header min-h-screen bg-gray-800">
                 <div className="grid place-items-center">
-                    <p className="text-gray-500">Brightness-Control</p>
-                    <AreaChart width={500} height={300} data={hundredValues}>
-                        <YAxis stroke={textColor}/>
+                    <p className="m-8 mt-9  text-4xl text-gray-500">Brightness-Control</p>
+                    <ComposedChart width={500} height={300} data={hundredValues}>
+                        <defs>
+                            <linearGradient id="brightness" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor={graphColor} stopOpacity={0.8}/>
+                                <stop offset="95%" stopColor={graphColor} stopOpacity={0}/>
+                            </linearGradient>
+                        </defs>
+                        <YAxis stroke={textColor}>
+                            <Label angle={-90} value="Brightness" fill={textColor} position='insideLeft'
+                                   style={{textAnchor: 'middle'}}/>
+                        </YAxis>
                         <XAxis dataKey="name" stroke={textColor}/>
-                        <Area dot={false} type="monotone" dataKey="x" stroke={textColor} fill={fillColor}/>
-                    </AreaChart>
-                    <div>
-                        <button className={buttonStyle} onClick={() => setBrightness(0.5)}>
-                            Set 0.5
-                        </button>
-                        <button className={buttonStyle} onClick={() => setBrightness(1)}>
-                            Set 1.0
-                        </button>
-                    </div>
+                        <Area dot={false} type="monotone" dataKey="x" stroke={textColor} fill="url(#brightness)"/>
+                    </ComposedChart>
+                    <Box width={500}>
+                        <Slider
+                            size="small"
+                            min={10}
+                            max={100}
+                            aria-label="Small"
+                            valueLabelDisplay="auto"
+                            value={brightnessValue}
+                            onChange={(e, value) => setBrightnessValue(value)}
+                            onChangeCommitted={(e, value) => setBrightness(value)}
+                        />
+                    </Box>
                 </div>
             </header>
         </div>
