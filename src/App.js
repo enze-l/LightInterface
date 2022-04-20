598447062f7c49bfc2e0e51407d19eb79df04541
enze-l <leon.enzenberger@protonmail.com>
2022-04-20 10:03:27 +0200

    Display sample graph

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import {LineChart, Line, XAxis, YAxis} from "recharts";
+
 function App() {
+  function convertArrayToObjects(array){
+    let objectArray = []
+    array.forEach(element => objectArray.push({uv: element}))
+    return objectArray
+  }
+
+  const data = convertArrayToObjects([1, 2, 3, 5, 4])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +23,11 @@ function App() {
         >
           Learn React
         </a>
+        <LineChart width={500} height={300} data={data}>
+          <XAxis/>
+          <YAxis/>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        </LineChart>
       </header>
     </div>
   );
