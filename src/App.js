e0c6e2c6a3172594dd709b3cb168d1ac37e1a16b
enze-l <leon.enzenberger@protonmail.com>
2022-04-20 13:03:56 +0200

    Display graph with buttons

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
-import {LineChart, Line, XAxis, YAxis} from "recharts";
+import {AreaChart, XAxis, YAxis, Area} from "recharts";
+import axios from "axios";
+import React from "react";
+
+const serverAddress = "http://localhost:3000"
+const textColor = "#6B7280"
+const fillColor = "#dbdbdb"
+const buttonStyle = "m-1 border border-gray-500 hover:bg-gray-700 text-gray-500 font-bold py-2 px-4 rounded-full"
 
 function App() {
-  function convertArrayToObjects(array){
-    let objectArray = []
-    array.forEach(element => objectArray.push({uv: element}))
-    return objectArray
-  }
+    const [hundredValues, setHundredValues] = React.useState([])
 
-  const data = convertArrayToObjects([1, 2, 3, 5, 4])
+    React.useEffect(() => {
+        axios.get(serverAddress + "/sensor/100")
+            .then(res => {
+                const array = String(res.data).split(/(\s+)/).filter(e => e.trim().length > 0)
+                setHundredValues(convertArrayToObjects(array))
+            })
+    }, [])
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <LineChart width={500} height={300} data={data}>
-          <XAxis/>
-          <YAxis/>
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        </LineChart>
-      </header>
-    </div>
-  );
+    function convertArrayToObjects(array) {
+        let objectArray = []
+        array.forEach(element => objectArray.push({name: element.index + 1, x: element}))
+        return objectArray
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header min-h-screen bg-gray-800">
+                <div className="grid place-items-center">
+                    <p className="text-gray-500">Brightness-Control</p>
+                    <AreaChart width={500} height={300} data={hundredValues}>
+                        <YAxis stroke={textColor}/>
+                        <XAxis dataKey="name" stroke={textColor}/>
+                        <Area dot={false} type="monotone" dataKey="x" stroke={textColor} fill={fillColor}/>
+                    </AreaChart>
+                    <div>
+                        <button className={buttonStyle}>
+                            Set 0.5
+                        </button>
+                        <button className={buttonStyle}>
+                            Set 1.0
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
