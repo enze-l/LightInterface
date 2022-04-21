9ef54c5e7145dd15882ecfd4699f3871a619246b
enze-l <leon.enzenberger@protonmail.com>
2022-04-21 17:30:26 +0200

    Add getter and setter for slider values

--- src/App.js
+++ src/App.js
@@ -8,32 +8,63 @@ const textColor = "#6B7280"
 const graphColor = "#ffffff"
 const maxValueColor = "#ffdd00"
 const currentValueColor = "#6faeff"
-const buttonStyle = "m-1 border border-gray-500 hover:bg-gray-700 text-gray-500 font-bold py-2 px-4 rounded-full"
 
 function App() {
     const [hundredValues, setHundredValues] = React.useState([])
     const [displayBrightnessRange, setDisplayBrightnessRange] = React.useState([10, 100])
     const [sensorBrightnessRange, setSensorBrightnessRange] = React.useState([1, 500])
 
+    //sensor values
     function getHundredValues() {
         return axios.get(serverAddress + "/sensor/100")
     }
-
-    function getCurrentDisplayBrightness() {
-        return axios.get(serverAddress + "/display/brightness")
+    function getDayValues() {
+        return axios.get(serverAddress + "/sensor/day")
     }
-
     function getCurrentSensorBrightness() {
         return axios.get(serverAddress + "/sensor")
     }
-
     function getMaxBrightness() {
         return axios.get(serverAddress + "/sensor/max")
     }
+    function setMinThreshold(value){
+        axios.post(serverAddress + "/sensor/threshold/min", value).then()
+    }
+    function getMinThreshold(){
+        return axios.get(serverAddress + "/sensor/threshold/min")
+    }
+    function setMaxThreshold(value){
+        axios.post(serverAddress + "/sensor/threshold/max", value).then()
+    }
+    function getMaxThreshold(){
+        return axios.get(serverAddress + "/sensor/threshold/max")
+    }
+
+    //client values
+    function getDisplayBrightness() {
+        return axios.get(serverAddress + "/display/brightness")
+    }
+    function setDisplayBrightness(dataPercent) {
+        axios.post(serverAddress + "/display/brightness", dataPercent[1] / 100).then()
+    }
+    function getDisplayMin() {
+        return axios.get(serverAddress + "/display/min")
+    }
+    function setDisplayMin(value){
+        axios.post(serverAddress + "/display/min", value).then()
+    }
+    function getDisplayMax() {
+        return axios.get(serverAddress + "/display/max")
+    }
+    function setDisplayMax(value){
+        axios.post(serverAddress + "/display/max", value).then()
+    }
+
+    
 
     React.useEffect(() => {
         Promise.all(
-            [getHundredValues(), getCurrentDisplayBrightness(), getMaxBrightness(), getCurrentSensorBrightness()]
+            [getHundredValues(), getDisplayBrightness(), getMaxBrightness(), getCurrentSensorBrightness()]
         ).then((results) => {
             const hundredValues = String(results[0].data).split(/(\s+)/).filter(e => e.trim().length > 0)
             const currentDisplayBrightness = results[1].data * 100
@@ -45,12 +76,6 @@ function App() {
             setDisplayBrightnessRange([20, currentDisplayBrightness])
         })
     }, [])
-
-    function setBrightness(dataPercent) {
-        axios.post(serverAddress + "/display/brightness", dataPercent[1] / 100).then(res => {
-            console.log(dataPercent)
-        })
-    }
 
     function convertArrayToObjects(array, max, current) {
         let objectArray = []
@@ -77,13 +102,12 @@ function App() {
                                 <Label angle={-90} value="Brightness" fill={textColor} position='insideLeft'
                                        style={{textAnchor: 'middle'}}/>
                             </YAxis>
-                            <Legend/>
                             <XAxis dataKey="brightness" stroke={textColor}/>
                             <Area dot={false} type="monotone" dataKey="day" stroke={textColor} fill="url(#brightness)"/>
                             <Line dot={false} type="monotone" dataKey="max" stroke={maxValueColor}/>
                             <Line dot={false} type="monotone" dataKey="current" stroke={currentValueColor}/>
                         </ComposedChart>
-                        <div className="pl-2 pb-14 pt-1">
+                        <div className="pl-8 pb-9 pt-2">
                             <Slider
                                 size="small"
                                 min={0}
@@ -97,16 +121,18 @@ function App() {
                         </div>
                     </div>
                     <Box width={500}>
-                        <Slider
-                            size="small"
-                            min={10}
-                            max={100}
-                            aria-label="Small"
-                            valueLabelDisplay="auto"
-                            value={displayBrightnessRange}
-                            onChange={(e, value) => setDisplayBrightnessRange(value)}
-                            onChangeCommitted={(e, value) => setBrightness(value)}
-                        />
+                        <div className="pl-9 pr-8">
+                            <Slider
+                                size="small"
+                                min={10}
+                                max={100}
+                                aria-label="Small"
+                                valueLabelDisplay="auto"
+                                value={displayBrightnessRange}
+                                onChange={(e, value) => setDisplayBrightnessRange(value)}
+                                onChangeCommitted={(e, value) => setDisplayBrightness(value)}
+                            />
+                        </div>
                     </Box>
                 </div>
             </header>
